70d8168b5824d82ce98297de277d8780277157ec
sunitaS407 <sahasunita401@gmail.com>
2025-08-07 21:53:00 +0530

    Added Resume PDF for download

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ function App() {
 
         {/* ✅ Resume Download Button */}
   <motion.a
-    href="../public/Sunita_Resume.pdf"
+    href="/Sunita_Resume.pdf"
     download
     whileHover={{ scale: 1.05 }}
     whileTap={{ scale: 0.95 }}
